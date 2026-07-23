96c0f84532a6c0e600712e735429ef7623db8e06
itsritesh02 <itsritesh02@gmail.com>
2026-07-23 14:29:52 +0530

    tripletsum

--- Arrays/TripletSum.java
+++ Arrays/TripletSum.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-public class PairSum {
-
+public class TripletSum {
   static int sum(int[] arr, int target) {
     int count = 0;
     for (int i = 0; i < arr.length; i++) {
       for (int j = i + 1; j < arr.length; j++) {
-        if (arr[i] + arr[j] == target) {
-          System.out.println("Indices are: " + i + " , " + j);
-          count++;
+        for (int k = j + 1; k < arr.length; k++) {
+          if (arr[i] + arr[j] + arr[k] == target) {
+            System.out.println("Indices are: " + i + " , " + j + " , " + k);
+            count++;
+          }
         }
       }
     }
@@ -36,7 +37,7 @@ public class PairSum {
 
     System.out.println("Enter target");
     int Target = sc.nextInt();
-int total=sum(arr, Target);
-    System.out.println("total pairs are :"+total);
+    int total = sum(arr, Target);
+    System.out.println("total triplets are :" + total);
   }
 }
