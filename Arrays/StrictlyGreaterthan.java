a10c17d4ef8a03b713ea0261bdda6ebb659c8a3f
itsritesh02 <itsritesh02@gmail.com>
2026-07-23 12:27:39 +0530

    StrictlyGreaterthan

---
+++ Arrays/StrictlyGreaterthan.java
@@ -0,0 +1,41 @@
+
+
+import java.util.Scanner;
+
+public class StrictlyGreaterthan {
+
+  static int StrictlyGT(int[] arr, int x) {
+    int count = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > x) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter x");
+    int x = sc.nextInt();
+
+    // Array ka size
+    System.out.println("Enter Sizeof Array");
+    int n = sc.nextInt();
+
+    // Array create
+    int[] arr = new int[n];
+
+    System.out.println("Enter Array Elements");
+
+    // Array input
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    int ans = StrictlyGT(arr, x);
+    System.out.println("x:" + ans);
+
+  }
+}
