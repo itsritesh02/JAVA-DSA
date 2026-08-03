f400762b2f66acda3d125e92cfbdc2bd10bb91bf
itsritesh02 <itsritesh02@gmail.com>
2026-08-03 15:11:24 +0530

    MoveEven

---
+++ Arrays/MoveEven.java
@@ -0,0 +1,36 @@
+public class MoveEven {
+
+  static void printArr(int arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  static void swapArr(int arr[], int i, int k) {
+
+    int temp = arr[i];
+    arr[i] = arr[k];
+    arr[k] = temp;
+
+  }
+
+  static void moveArr(int[] arr) {
+    int k = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 == 0) {
+
+        swapArr(arr, i, k);
+        k++;
+      }
+    }
+
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 1, 2, 3, 4, 5, 6 };
+    printArr(arr);
+    moveArr(arr);
+    printArr(arr);
+  }
+}
